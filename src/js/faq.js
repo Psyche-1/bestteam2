5676117e8b2cfe08e4805b48ce4ca99077f86c7c
Psyche1 <vovchik357@gmail.com>
2024-12-21 16:30:12 +0200

    add arrow directions

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,3 +2,20 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 const accordion = new Accordion('.accordion-container', { showMultiple: true });
+
+const arrows = document.querySelectorAll('.ac-trigger');
+
+for (const arrow of arrows) {
+  arrow.addEventListener('click', changeDirectionArrow);
+}
+
+function changeDirectionArrow(e) {
+  console.log(e.currentTarget.getAttribute('aria-expanded')); //.children[0].children[0]
+  if (e.currentTarget.getAttribute('aria-expanded') === 'true') {
+    e.currentTarget.children[0].children[0].innerHTML =
+      '<use href="../img/icons.svg#icon-icon-up"></use>';
+  } else if (e.currentTarget.getAttribute('aria-expanded') === 'false') {
+    e.currentTarget.children[0].children[0].innerHTML =
+      '<use href="../img/icons.svg#icon-icon-down"></use>';
+  }
+}
