b4457e8903361e5bf7482c27289624e8f6e9d4be
DorianNet93 <lifebsfg@gmail.com>
2024-12-22 16:24:33 +0200

    backdrop

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,14 +1,19 @@
 const form = document.getElementById('form-together');
 const modal = document.getElementById('successModal');
+const modalOverlay = document.getElementById('modalOverlay');
 const modalMessage = document.getElementById('modalMessage');
 const closeModal = document.getElementById('closeModal');
 
+function closeModalWindow() {
+  modal.classList.remove('active'); 
+  modalOverlay.classList.remove('active');
+}
+
 form.addEventListener('submit', async (e) => {
-  e.preventDefault();
+  e.preventDefault(); 
 
   const emailInput = document.getElementById('together-input--email');
   const commentsInput = document.getElementById('together-input--comments');
-
   const data = {
     email: emailInput.value,
     comment: commentsInput.value,
@@ -30,10 +35,10 @@ form.addEventListener('submit', async (e) => {
         <span class="thank-you-text--two">${responseData.message}</span>
       `;
       modal.classList.add('active');
-      form.reset();
+      modalOverlay.classList.add('active'); 
+      form.reset(); 
     } else {
       let errorMessage = 'An error occurred while sending data. Try again.';
-      
       if (response.status === 400) {
         errorMessage = 'Bad request: Invalid request body.';
       } else if (response.status === 404) {
@@ -41,17 +46,18 @@ form.addEventListener('submit', async (e) => {
       } else if (response.status === 500) {
         errorMessage = 'Server error: Please try again later.';
       }
-
       modalMessage.textContent = errorMessage;
       modal.classList.add('active');
+      modalOverlay.classList.add('active');
     }
   } catch (error) {
-    console.error('Fetch Error:', error);
-    modalMessage.textContent = 'An error occurred while sending data. Try again.';
+    console.error('Fetch Error:', error.message);
+    modalMessage.textContent = 'Fetch Error';
     modal.classList.add('active');
+    modalOverlay.classList.add('active');
   }
 });
 
-closeModal.addEventListener('click', () => {
-  modal.classList.remove('active');
-});
+closeModal.addEventListener('click', closeModalWindow);
+
+modalOverlay.addEventListener('click', closeModalWindow);
