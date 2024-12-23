f8f5d040e255252551c7b66ea8ed47e7bfd1668e
artem <artemilchenko28@gmail.com>
2024-12-23 19:45:42 +0100

    header created

--- src/js/header.js
+++ src/js/header.js
@@ -1,23 +1,22 @@
 const button = document.querySelector(".header_menu_img");
 
-button.addEventListener("click", () => { 
-    document.querySelector(".modal_window").classList.add("opened");
-})
+button.addEventListener("click", () => {
+    document.querySelector(".modal_window").style.display = "flex";
+});
 
 const close = document.querySelector(".x_icon");
-close.addEventListener("click", () => { 
-    document.querySelector(".modal_window").classList.remove("opened");
-})
+close.addEventListener("click", () => {
+    document.querySelector(".modal_window").style.display = "none";
+});
 
-const modal_menu = document.querySelector(".modal_menu");
-modal_menu.addEventListener("click", (event) => { 
-    document.querySelector(".modal_window").classList.remove("opened");
 
-})
+
 const nav_link = document.querySelector(".nav_link");
-nav_link.addEventListener("click", () => { 
+nav_link.addEventListener("click", () => {
     document.querySelector(".menu").classList.remove("active");
-})
+});
+
+
 const menu_trigger = document.querySelector('.menu_trigger');
 
 menu_trigger.addEventListener('click', function () {
